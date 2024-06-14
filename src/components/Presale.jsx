004635ffb855e18450e7b177128c538f196fd23e
Cappool <cpdps00@gmail.com>
2024-06-14 22:02:53 +0530

    mod

--- src/components/Presale.jsx
+++ src/components/Presale.jsx
@@ -164,7 +164,7 @@ const Presale = () => {
     setCurrentStep(Number(getCurrentStep));
     setRoundDetails(getRoundDetails);
     setRoundTimeDetails(getRoundTimeDetails);
-    setUsdraised(Number(getUSDraised) / 10 ** 18 + 7.946319 * 10 ** 4);
+    setUsdraised(Number(getUSDraised) / 10 ** 18 + 8.194631 * 10 ** 4);
   }, [
     getLatestPrice,
     getCoinPrice,
